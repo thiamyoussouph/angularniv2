189b5c532c5dc174db994701588dd065343006c3
thiamyoussouph <thiamasjo@gmail.com>
2025-08-05 13:12:59 +0000

    managemnane ngfor

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,20 +14,23 @@ import { Footer } from "./footer/footer";
 })
 export class App {
   protected readonly title = signal('senjus');
-tabarticle=[
- {  titre: 'jus d or', description: 'Un jus d or frais et vitaminé', image: 'assets/jusdorange.jpg', alt: 'jus d or' }
-, 
- {  titre: 'jus de mangue', description: 'Un jus de mangue sucré et rafraîchissant', image: 'assets/imagmg.jpg', alt: 'jus de mangue' },
- {  titre: 'jus de bisap', description: 'Un jus de bisap délicieux et naturel', image: 'assets/jusbisp.jpg', alt: 'jus de bisap' },
- {  titre: 'jus d gingembre', description: 'Un jus d gingembre épicé et énergisant', image: 'assets/jusgin.jpg', alt: 'jus d gingembre' }
-]
+  tabarticle = [
+    { titre: 'jus d or', description: 'Un jus d or frais et vitaminé', image: 'assets/jusdorange.jpg', alt: 'jus d or',prix:1500 ,dispo:true }
+    ,
+    { titre: 'jus de mangue', description: 'Un jus de mangue sucré et rafraîchissant', image: 'assets/imagmg.jpg', alt: 'jus de mangue',prix:2000 ,dispo:false},
+     { titre: 'jus de bisap', description: 'Un jus de bisap délicieux et naturel', image: 'assets/jusbisp.jpg', alt: 'jus de bisap' ,prix:2500 ,dispo:true},
+    { titre: 'jus d gingembre', description: 'Un jus d gingembre épicé et énergisant', image: 'assets/jusgin.jpg', alt: 'jus d gingembre' ,prix:3000 ,dispo:true}
+    ,   { titre: 'jus de mangue', description: 'Un jus de mangue sucré et rafraîchissant', image: 'assets/imagmg.jpg', alt: 'jus de mangue' ,prix:2000 ,dispo:false},
+     { titre: 'jus de bisap', description: 'Un jus de bisap délicieux et naturel', image: 'assets/jusbisp.jpg', alt: 'jus de bisap' ,prix:2500 ,dispo:true},
+    { titre: 'jus d gingembre', description: 'Un jus d gingembre épicé et énergisant', image: 'assets/jusgin.jpg', alt: 'jus d gingembre' ,prix:3000 ,dispo:true}
+  ]
 
-  name:string="moussa";
-message: string = '';
-getmessage(event: string) {
-   this.message="merci d avoir liker "+event;
-  console.log(event);
-}
+  name: string = "moussa";
+  message: string = '';
+  getmessage(event: string) {
+    this.message = "merci d avoir liker " + event;
+    console.log(event);
+  }
 
 }
 
